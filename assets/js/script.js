8986d41146c1b2d67bc77ffcf49685a3eea9b1d8
Bo De Groot <bclm.degroot@gmail.com>
2023-11-23 13:51:47 +0100

    Added the needs bubble for the monster, still need to add JS for this

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,22 @@
 // let for buttons
 let startButton = document.getElementById("start-button");
+let endButton = document.getElementById("end-button");
 let drinkButton = document.getElementById("drink");
 let eatButton = document.getElementById("eat");
 let petButton = document.getElementById("pet");
 let playButton = document.getElementById("play");
-let endButton = document.getElementById("end-button");
 
 // let for page change
 let startPage = document.getElementsByClassName("start-page");
 let playPage = document.getElementsByClassName("play-page");
 let endPage = document.getElementsByClassName("end-page");
 
-// Hides the other pages on startup
+// Toggles the other "pages" on startup
 document.querySelector(".play-page").classList.toggle("hide");
 document.querySelector(".end-page").classList.toggle("hide");
 
-// Start button event and funtion
-startButton.addEventListener("click", function() {
-	startGame();
+// Start page
+startButton.addEventListener("click", function () {
+    startGame();
 });
 
-function startGame() {
-    document.querySelector(".start-page").classList.toggle("hide");
-    document.querySelector(".play-page").classList.toggle("show");
-}
