39dd6796eba1603113a6550a8dfe0b22adafed2d
Bo de Groot <bclm.degroot@gmail.com>
2023-11-28 18:49:35 +0100

    changed id, class and function names for better clarification

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 // let for buttons
 let startButton = document.getElementById("start-button");
 let endButton = document.getElementById("end-button");
-let drinkButton = document.getElementById("drink");
-let eatButton = document.getElementById("eat");
-let loveButton = document.getElementById("love");
-let playButton = document.getElementById("play");
+let waterButton = document.getElementById("water-button");
+let foodButton = document.getElementById("food-button");
+let loveButton = document.getElementById("love-button");
+let playButton = document.getElementById("play-button");
 
-// let for page change
+// let for page changes
 let startPage = document.getElementsByClassName("start-page");
 let playPage = document.getElementsByClassName("play-page");
 let endPage = document.getElementsByClassName("end-page");
+
+// let for game statistics
 let score = 0;
 let gameOver = false;
 
@@ -29,19 +31,19 @@ function startGame() {
 
     // Play game page
     // Needs bubble
-    let monsterNeeds = document.querySelectorAll(".need-icon")
-    let needs = getRandomInt(4);
+    let needIcon = document.getElementsByClassName("need-icon");
+    let needNumber = getRandomInt(4);
 
-    function needBubble(){
-        showAndHideBubble(needs);
+    function needBubble() {
+        showAndHideBubble(needNumber);
     }
 
-    function showAndHideBubble(needs) {
-        for (let i = 0; i < monsterNeeds.length; i++){
-            if (i === needs){
-                monsterNeeds[i].classList.remove("none")
+    function showAndHideBubble(needNumber) {
+        for (let i = 0; i < needIcon.length; i++) {
+            if (i === needNumber) {
+                needIcon[i].classList.remove("none")
             } else {
-                monsterNeeds[i].classList.add("none")
+                needIcon[i].classList.add("none")
             }
         }
     }
@@ -49,5 +51,7 @@ function startGame() {
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
+
+    needBubble();
 
 }
