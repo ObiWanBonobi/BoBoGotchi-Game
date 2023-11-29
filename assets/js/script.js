38d92e67fc39f6431b159dbf6f5b49e28e7c0383
Bo de Groot <bclm.degroot@gmail.com>
2023-11-29 11:53:36 +0100

    changed the need bubble so that it randomises an img and some layout changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// let for buttons
+// Buttons
 let startButton = document.getElementById("start-button");
 let endButton = document.getElementById("end-button");
 let waterButton = document.getElementById("water-button");
@@ -6,52 +6,43 @@ let foodButton = document.getElementById("food-button");
 let loveButton = document.getElementById("love-button");
 let playButton = document.getElementById("play-button");
 
-// let for page changes
+// Page changes
 let startPage = document.getElementsByClassName("start-page");
 let playPage = document.getElementsByClassName("play-page");
 let endPage = document.getElementsByClassName("end-page");
 
-// let for game statistics
+// Game statistics
 let score = 0;
 let gameOver = false;
+
+// Images array
+let needIcon = document.querySelector("#need-icon");
+let iconArray = [
+        "assets/images/pet-water.webp",
+        "assets/images/pet-food.webp",
+        "assets/images/pet-love.webp",
+        "assets/images/pet-play.webp"
+];
+
+function changeImg(needIcon,iconArray) {
+  setInterval(function() {
+    let randomNum = Math.floor(Math.random() * iconArray.length);
+    needIcon.src = iconArray[randomNum];
+  },1500);
+}
 
 // Toggles the other "pages" on startup
 document.querySelector(".play-page").classList.toggle("none");
 document.querySelector(".end-page").classList.toggle("none");
 
 // Start page
-startButton.addEventListener("click", function () {
-    startGame();
-});
+startButton.addEventListener("click", startGame);
 
 function startGame() {
     // Shows the Play page and hides the start page
     document.querySelector(".start-page").classList.toggle("none");
     document.querySelector(".play-page").classList.toggle("none");
 
-    // Play game page
-    // Needs bubble
-    let needIcon = document.getElementsByClassName("need-icon");
-    let needNumber = getRandomInt(4);
-
-    function needBubble() {
-        showAndHideBubble(needNumber);
-    }
-
-    function showAndHideBubble(needNumber) {
-        for (let i = 0; i < needIcon.length; i++) {
-            if (i === needNumber) {
-                needIcon[i].classList.remove("none")
-            } else {
-                needIcon[i].classList.add("none")
-            }
-        }
-    }
-
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
-
-    needBubble();
-
+    // Need bubble
+    changeImg(needIcon,iconArray);
 }
