1c860f8663981025c3dff0b70595d740ede2c3f0
Bo De Groot <bclm.degroot@gmail.com>
2023-12-04 20:42:51 +0100

    Fixed syntax mistakes and fixed the red need bubble function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,58 @@
-// Buttons
-let startButton = document.getElementById("start-button");
-let endButton = document.getElementById("end-button");
-let waterButton = document.getElementById("water-button");
-let foodButton = document.getElementById("food-button");
-let loveButton = document.getElementById("love-button");
-let playButton = document.getElementById("play-button");
+// Play buttons
+const startButton = document.getElementById("start-button");
+const endButton = document.getElementById("end-button");
+const waterButton = document.getElementById("water-button");
+const foodButton = document.getElementById("food-button");
+const loveButton = document.getElementById("love-button");
+const playButton = document.getElementById("play-button");
 
 // Page changes
-let startPage = document.getElementsByClassName("start-page");
-let playPage = document.getElementsByClassName("play-page");
-let endPage = document.getElementsByClassName("end-page");
+const startPage = document.getElementsByClassName("start-page");
+const playPage = document.getElementsByClassName("play-page");
+const endPage = document.getElementsByClassName("end-page");
 
 // Game statistics
 let score = 0;
 let gameOver = false;
 
-// Images array and randomizer function
-let needIcon = document.querySelector("#need-icon");
-let iconArray = [
+// Images array for red need bubble
+let redNeedIcon = document.querySelector("#need-icon");
+const ICONS = [
     "assets/images/pet-water.webp",
     "assets/images/pet-food.webp",
     "assets/images/pet-love.webp",
     "assets/images/pet-play.webp"
 ];
 
-function changeImg(needIcon, iconArray) {
-    setInterval(function () {
-        let randomNum = Math.floor(Math.random() * iconArray.length);
-        needIcon.src = iconArray[randomNum];
-    }, 2000);
-}
+// Function that toggles between a blank icon and a randomly selected icon from the ICONS array
+function randomIconInRedBubble() {
+    let selectedElement = selectArrayElement(ICONS);
+    redNeedIcon.src = "assets/images/blank.png";
 
-function pauseImg() {
-    setInterval(function () {
-        needIcon.src = "assets/images/blank.png";
-    }, 500);
+    setTimeout(function() {
+        redNeedIcon.src = ICONS[selectedElement];
+        setTimeout(randomIconInRedBubble, 2000);
+    }, 700);
+} 
+
+// Selects a random value in an array
+function selectArrayElement(array) {
+    let randomNum = Math.floor(Math.random() * array.length);
+    return randomNum;
 }
 
 // Toggles the other "pages" on startup
 document.querySelector(".play-page").classList.toggle("none");
 document.querySelector(".end-page").classList.toggle("none");
 
-// Start page
+// Start page button
 startButton.addEventListener("click", startGame);
 
 function startGame() {
-    // Shows the Play page and hides the start page
+    // Shows the Play page, hides the start page and starts the red need bubble with a blank icon
     document.querySelector(".start-page").classList.toggle("none");
     document.querySelector(".play-page").classList.toggle("none");
 
-    // Need bubble
-    changeImg(needIcon, iconArray);
+    // Need bubble where the images toggles between an icon and a blank img
+    randomIconInRedBubble();
 }
