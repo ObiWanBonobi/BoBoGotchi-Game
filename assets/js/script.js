c6ab26653344d3e7fe36454372f9b194942160f5
Bo De Groot <bclm.degroot@gmail.com>
2023-12-06 20:09:53 +0100

    added the controlls for the play buttons, not yet finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,8 @@ const playPage = document.getElementsByClassName("play-page");
 const endPage = document.getElementsByClassName("end-page");
 
 // Game statistics
+let score = document.getElementsByClassName(".score");
 let result = 0;
-let gameOver = false;
 
 // Images array for red need bubble
 let redNeedIcon = document.querySelector("#need-icon");
@@ -27,7 +27,7 @@ function selectArrayElement(array) {
 }
 
 // Toggles between a blank icon and a randomly selected icon from the ICONS array
-let currentSelectedElement = '';
+let currentSelectedElement = "";
 
 function randomIconInRedBubble() {
     let selectedElement = selectArrayElement(ICONS);
@@ -40,6 +40,69 @@ function randomIconInRedBubble() {
     }, 2000);
 }
 
+// Function that plays the game
+function playTheGame() {
+    // Starts changing the random Icon bubble
+    randomIconInRedBubble();
+
+    // Controls buttons for the game
+    let pressedButton = '';
+    let correctButtonClicked = false;
+
+    // Water button
+    document.getElementById("0").addEventListener("click", function () {
+        pressedButton = document.getElementById("0").id;
+        correctButtonClicked = checkPressedButton(pressedButton, currentSelectedElement);
+        if (correctButtonClicked == true) {
+            console.log('point')
+        } else {
+            console.log('dead')
+        }
+    });
+
+    // Food button
+    document.getElementById("1").addEventListener("click", function () {
+        pressedButton = document.getElementById("1").id;
+        correctButtonClicked = checkPressedButton(pressedButton, currentSelectedElement);
+        if (correctButtonClicked == true) {
+            console.log('point')
+        } else {
+            console.log('dead')
+        }
+    });
+
+    // Love button
+    document.getElementById("2").addEventListener("click", function () {
+        pressedButton = document.getElementById("2").id;
+        correctButtonClicked = checkPressedButton(pressedButton, currentSelectedElement);
+        if (correctButtonClicked == true) {
+            console.log('point')
+        } else {
+            console.log('dead')
+        }
+    });
+
+    // Play button
+    document.getElementById("3").addEventListener("click", function () {
+        pressedButton = document.getElementById("3").id;
+        correctButtonClicked = checkPressedButton(pressedButton, currentSelectedElement);
+        if (correctButtonClicked == true) {
+            console.log('point')
+        } else {
+            console.log('dead')
+        }
+    });
+}
+
+// Check if the clicked button is correct
+function checkPressedButton(pressedButton, currentIcon) {
+    if (pressedButton == currentIcon) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 // Start page button
 startButton.addEventListener("click", startGame);
 
@@ -48,12 +111,14 @@ function startGame() {
     document.querySelector(".start-page").classList.toggle("none");
     document.querySelector(".play-page").classList.toggle("none");
 
-    // Need bubble where the images toggles between an icon and a blank img
-    randomIconInRedBubble();
+    // plays the game
+    playTheGame();
 }
 
+// Restart page button
 endButton.addEventListener("click", restartGame);
 
+// Reloads the page to show the start page again
 function restartGame() {
     location.reload();
 }
